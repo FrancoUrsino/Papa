16cef9677bec4de62ffb1661a0ca7601a5ac26ab
Franco Ursino <franncoursinno@gmail.com>
2023-06-25 19:07:55 -0300

    .

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,3 @@
-// const menuBtn = document.querySelector('#menuBtn');
-
-// menuBtn.addEventListener('click', function (menu) {
-//   document.querySelector('body').classList.toggle('MobileNavOpen');
-//   this.classList.toggle('fa-xmark')
-// })
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
