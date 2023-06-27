41b1721f427af2e639ca871f98f3a4872a2d1230
Franco Ursino <franncoursinno@gmail.com>
2023-06-26 22:29:42 -0300

    ..

--- js/about.js
+++ js/about.js
@@ -7,13 +7,15 @@ const showContainer = () => {
   
   containerContent.innerHTML =
   `
-  <div class="about__container--2--container--1">
-    <button class="about__container--2--container--1--btn active" id="btn1">¿Qué hacemos?</button>
-    <button class="about__container--2--container--1--btn" id="btn2">¿Qué utilizamos?</button>
-  </div>
-  <div class="about__container--2--container--2">
-    <p class="about__container--2--container--2--text" id="text1" style="display: none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minima quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi.</p>
-    <p class="about__container--2--container--2--text" id="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minimaLorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minima quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi. quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi.</p>
+  <div class="about__container--2--container--container">
+    <div class="about__container--2--container--container--1">
+      <button class="about__container--2--container--container--1--btn active" id="btn1">¿Qué hacemos? <i class="fa-sharp fa-solid fa-block-brick"></i> <i class="fa-solid fa-helmet-safety"></i></button>
+      <button class="about__container--2--container--container--1--btn" id="btn2">¿Qué usamos? <i class="fa-solid fa-hammer"></i></button>
+    </div>
+    <div class="about__container--2--container--container--2">
+      <p class="about__container--2--container--container--2--text" id="text1" style="display: none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minima quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi.</p>
+      <p class="about__container--2--container--container--2--text" id="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minimaLorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minima quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi. quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi.</p>
+    </div>
   </div>
   `
   container.append(containerContent);
@@ -24,8 +26,8 @@ const showContainer = () => {
   const softText = document.querySelector('#text2');
 
 
-  const changeText2 = btn2.addEventListener('click',() =>{text2.style.display = "none", text1.style.display = "block", btn2.classList.add('active'), btn1.classList.remove('active')});
-  const changeText1 = btn1.addEventListener('click',() =>{text1.style.display = "none", text2.style.display = "block", btn1.classList.add('active'), btn2.classList.remove('active')});
+  const changeText2 = btn2.addEventListener('click',() =>{text2.style.display = "none", text1.style.display = "flex", btn2.classList.add('active'), btn1.classList.remove('active')});
+  const changeText1 = btn1.addEventListener('click',() =>{text1.style.display = "none", text2.style.display = "flex", btn1.classList.add('active'), btn2.classList.remove('active')});
 
   
 }
