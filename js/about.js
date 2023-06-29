ec029d2463afa108282c791d431603fdab3f3166
Franco Ursino <franncoursinno@gmail.com>
2023-06-29 11:55:04 -0300

    finish about section

--- js/about.js
+++ js/about.js
@@ -9,8 +9,8 @@ const showContainer = () => {
   `
   <div class="about__container--2--container--container">
     <div class="about__container--2--container--container--1">
-      <button class="about__container--2--container--container--1--btn active" id="btn1">¿Qué hacemos? <i class="fa-sharp fa-solid fa-block-brick"></i> <i class="fa-solid fa-helmet-safety"></i></button>
-      <button class="about__container--2--container--container--1--btn" id="btn2">¿Qué usamos? <i class="fa-solid fa-hammer"></i></button>
+      <button class="about__container--2--container--container--1--btn active" id="btn1">¿Qué hacemos? <br> <i class="fa-sharp fa-solid fa-block-brick"></i> <i class="fa-solid fa-helmet-safety"></i></button>
+      <button class="about__container--2--container--container--1--btn" id="btn2">¿Qué utilizamos? <br> <i class="fa-solid fa-hammer"></i></button>
     </div>
     <div class="about__container--2--container--container--2">
       <p class="about__container--2--container--container--2--text" id="text1" style="display: none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quia, minima quidem dignissimos id numquam, at cupiditate explicabo nostrum fuga, quos porro modi.</p>
@@ -22,8 +22,8 @@ const showContainer = () => {
   
   const btn2 = document.querySelector('#btn2');
   const btn1 = document.querySelector('#btn1');
-  const hardText = document.querySelector('#text1');
-  const softText = document.querySelector('#text2');
+  const text1 = document.querySelector('#text1');
+  const text2 = document.querySelector('#text2');
 
 
   const changeText2 = btn2.addEventListener('click',() =>{text2.style.display = "none", text1.style.display = "flex", btn2.classList.add('active'), btn1.classList.remove('active')});
